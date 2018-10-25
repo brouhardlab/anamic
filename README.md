9b59e453e14b8097c288161ed45a3461fbe2e2a1
Hadrien Mary <hadrien.mary@gmail.com>
2018-10-25 16:51:32 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Anamic
+[![Binder](https://mybinder.org/badge.svg)](https://mybinder.org/v2/gh/hadim/anamic/master?urlpath=lab/tree/notebooks)
+[![Build Status](https://travis-ci.org/hadim/anamic.svg?branch=master)](https://travis-ci.org/hadim/anamic)
+
+Simulate, fit and analyze microtubules.
+
+# License
+
+Under BSD license. See [LICENSE](LICENSE).
+
+# Authors
+
+- Claire Edrington <>
+- Hadrien Mary <hadrien.mary@gmail.com>
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name="anamic",
+      version="0.1.0",
+      author='Hadrien Mary',
+      author_email='hadrien.mary@gmail.com',
+      url='https://github.com/hadim/anamic/',
+      description='Simulate, fit and analyze microtubules.',
+      include_package_data=True,
+      packages=find_packages(),
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Natural Language :: English',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3'])
