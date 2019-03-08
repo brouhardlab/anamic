4cc2986b4b93b6c41e2a182f1b722bce94afd7aa
Hadrien Mary <hadrien.mary@gmail.com>
2019-03-08 12:51:15 -0500

    Support LSM file for metadata>

--- anamic/imaging/metadata.py
+++ anamic/imaging/metadata.py
@@ -2,17 +2,22 @@ import tifffile
 
 
 def get_pixel_size(fname):
-    """Return the pixel size from the ImageJ metadata."""
+    """Return the pixel size in um."""
     tiff_obj = tifffile.TiffFile(str(fname))
     description = tiff_obj.pages.pages[0].description
 
-    # First method
+    # From LSM metadata
+    if tf.is_lsm:
+        pixel_size_m = tf.lsm_metadata['VoxelSizeX']
+        return pixel_size_m / 1e-6
+
+    # From ImageJ description
     scales = list(filter(lambda x: x.startswith('scales'), description.split("\n")))
     if len(scales) >= 1:
         scales = scales[0]
         pixel_size = scales.split('=')[1].split(',')[0]
         return float(pixel_size)
 
-    # Second method
+    # From ImageJ tags
     raw_pixel_size, mul = tiff_obj.pages.pages[0].tags['YResolution'].value
     return 1 / (raw_pixel_size) * mul
